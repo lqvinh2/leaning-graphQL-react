c9b82201f1618aa9bcd668c0ecf9c2f26a4076cb
Lâm Quang Vinh <lqvinh.hsu@gmail.com>
2022-09-30 21:54:02 +0700

    graphQL DATA có tham chiếu 1 book sẽ tìm được Author nào, tham khao query_graphQL_test.txt - P2

--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -9,7 +9,19 @@ const resolvers = {
         book: (parent, args) => books.find(x => x.id == args.id ),
         authors: (parent, args) => authors,
         author : (parent, args) => authors.find(x => x.id == args.id ),
-    }
+    },
+
+
+    // handle [field author] in [type Book] schema.js
+    Book: {
+		author: async (parent, args) => authors.find(x => x.id == parent.authorId ),
+    },
+
+    // handle [field books] in [type Author] schema.js
+    Author: {
+		books: async (parent, args) => books.filter(x => x.authorId == parent.id ),
+    },
+
 }
 
 module.exports = resolvers
